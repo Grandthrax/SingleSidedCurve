de2cb733649338054bc95fadead7ba8ad28a8667
jmonteer <jmonteer@protonmail.com>
2021-05-19 00:13:04 +0200

    feat: formatting

--- tests/test_operation_susd.py
+++ tests/test_operation_susd.py
@@ -2,7 +2,7 @@ from itertools import count
 from brownie import Wei, reverts, Contract, accounts, web3
 import eth_abi
 from brownie.convert import to_bytes
-from useful_methods import genericStateOfStrat,genericStateOfVault
+from useful_methods import genericStateOfStrat, genericStateOfVault
 import random
 import brownie
 
@@ -12,48 +12,71 @@ import brownie
 #           - change in loading (from low to high and high to low)
 #           - strategy operation at different loading levels (anticipated and "extreme")
 
-def test_susd_1(currency, susd, live_susd_vault,stratms, strategy_saave, yvaultv2SAAVE, Strategy,curvePool, accounts, hCRV,yvaultv2, orb,rewards,chain,yhbtcstrategyv2,live_wbtc_vault, ychad, whale,gov,strategist, interface):
+
+def test_susd_1(
+    currency,
+    susd,
+    live_susd_vault,
+    stratms,
+    strategy_saave,
+    yvaultv2SAAVE,
+    Strategy,
+    curvePool,
+    accounts,
+    hCRV,
+    yvaultv2,
+    orb,
+    rewards,
+    chain,
+    yhbtcstrategyv2,
+    live_wbtc_vault,
+    ychad,
+    whale,
+    gov,
+    strategist,
+    interface,
+):
     currency = susd
     decimals = currency.decimals()
     gov = stratms
-    
+
     vault = live_susd_vault
     gov = accounts.at(vault.governance(), force=True)
     strategy = strategy_saave
 
     yvault = yvaultv2SAAVE
-    amount = 1_000_000*1e18
+    amount = 1_000_000 * 1e18
     amounts = [0, 0, amount]
     print("curveid: ", strategy.curveId())
     print("slip: ", strategy._checkSlip(amount))
-    print("expectedOut: ", amount/strategy.virtualPriceToWant())
+    print("expectedOut: ", amount / strategy.virtualPriceToWant())
     print("curve token: ", strategy.curveToken())
     print("ytoken: ", strategy.yvToken())
-    yvault.setDepositLimit(2 **256 -1 , {'from': yvault.governance()})
-    vault.setDepositLimit(2 **256 -1 , {'from': gov})
+    yvault.setDepositLimit(2 ** 256 - 1, {"from": yvault.governance()})
+    vault.setDepositLimit(2 ** 256 - 1, {"from": gov})
 
     currency.approve(vault, 2 ** 256 - 1, {"from": whale})
     whale_before = currency.balanceOf(whale)
     whale_deposit = 1_000_000 * (10 ** (decimals))
     idl = Strategy.at(vault.withdrawalQueue(0))
-    idl.harvest({'from': gov})
-    chain.sleep(6*3600+1)
+    idl.harvest({"from": gov})
+    chain.sleep(6 * 3600 + 1)
     chain.mine(1)
 
     vault.deposit(whale_deposit, {"from": whale})
     vault.setManagementFee(0, {"from": gov})
 
-    vault.updateStrategyDebtRatio(idl, 0 , {"from": gov})
+    vault.updateStrategyDebtRatio(idl, 0, {"from": gov})
     before = vault.totalAssets()
 
     debt_ratio = 10_000
     vault.addStrategy(strategy, debt_ratio, 0, 2 ** 256 - 1, 1000, {"from": gov})
-    idl.harvest({'from': gov})
-    chain.sleep(6*3600+1)
+    idl.harvest({"from": gov})
+    chain.sleep(6 * 3600 + 1)
     chain.mine(1)
 
-    strategy.harvest({'from': strategist})
-    chain.sleep(6*3600+1)
+    strategy.harvest({"from": strategist})
+    chain.sleep(6 * 3600 + 1)
     chain.mine(1)
     genericStateOfStrat(strategy, currency, vault)
     genericStateOfVault(vault, currency)
@@ -67,24 +90,27 @@ def test_susd_1(currency, susd, live_susd_vault,stratms, strategy_saave, yvaultv
     yStrat.harvest({"from": vGov})
     chain.sleep(21600)
     chain.mine(1)
-    strategy.harvest({'from': strategist})
-    chain.sleep(3600*6+1)
+    strategy.harvest({"from": strategist})
+    chain.sleep(3600 * 6 + 1)
     chain.mine(1)
     genericStateOfStrat(strategy, currency, vault)
     genericStateOfVault(vault, currency)
-    print("\nEstimated APR: ", "{:.2%}".format(((vault.totalAssets()-before))/(before)))
+    print(
+        "\nEstimated APR: ",
+        "{:.2%}".format(((vault.totalAssets() - before)) / (before)),
+    )
 
     vault.withdraw(vault.balanceOf(whale), whale, 500, {"from": whale})
     whale_after = currency.balanceOf(whale)
-    print("profit =", (whale_after - whale_before)/(10 ** (decimals)))
+    print("profit =", (whale_after - whale_before) / (10 ** (decimals)))
     print("balance left =", vault.balanceOf(whale))
     genericStateOfStrat(strategy, currency, vault)
     genericStateOfVault(vault, currency)
     chain.sleep(21600)
     chain.mine(1)
 
-    strategy.harvest({'from': strategist})
-    chain.sleep(3600*6+1)
+    strategy.harvest({"from": strategist})
+    chain.sleep(3600 * 6 + 1)
     chain.mine(1)
 
     genericStateOfStrat(strategy, currency, vault)
